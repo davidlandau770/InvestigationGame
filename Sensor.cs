59e4ad9571aad0b58579b43b071c392c85401bff
davidlandau770 <dal778770@gmail.com>
2025-06-15 11:08:54 +0300

    add sensor

---
+++ Sensor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvestigationGame
+{
+    internal class Sensor
+    {
+        public string[] SensorsType { get; private set; }
+        
+        public Sensor()
+        {
+            SensorsType = GetRandomSensorType();
+        }
+
+        public string[] GetRandomSensorType()
+        {
+            string[] arrSensorType = { "basic", "termi" };
+            Random random = new Random();
+            string[] returnArrSensorType = { arrSensorType[random.Next(0, 1)], arrSensorType[random.Next(0, 1)] };
+            return arrSensorType;
+        }
+
+    }
+}
